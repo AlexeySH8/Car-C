2856dcf385031fbce57222b3bfe547a62737363d
AlexeySH8 <shevchenkowe@gmail.com>
2025-06-07 15:33:06 +0500

    EditToggle

--- Scripts/Game Managers/FilterManager.cs
+++ Scripts/Game Managers/FilterManager.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 public class FilterManager : MonoBehaviour
 {
     private static string SCAN_LINE_NUMBER = "_Number_Of_Scan_Lines";
+    private static string PREF_KEY = "UseCRTFilter";
 
     [SerializeField] private Toggle _toggle;
     [SerializeField] private Material _crtMaterial;
     [SerializeField] private VolumeProfile _crtProfile;
-    [SerializeField] private VolumeProfile _defaultProfile;
+    [SerializeField] private VolumeProfile _defaultProfile; 
 
     private float _scanLinesNumberCRT = 200f;
     private float _scanLinesNumberDefault = 0f;
@@ -18,23 +19,16 @@ public class FilterManager : MonoBehaviour
     private void Awake()
     {
         _volume = GetComponent<Volume>();
+        LoadFilterPrefs();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
-            SetCRTFilter();
-        if (Input.GetKeyDown(KeyCode.F))
-            SetDefaultFilter();
-    }
-
-    public void SetCRTFilter()
+    private void SetCRTFilter()
     {
         _crtMaterial.SetFloat(SCAN_LINE_NUMBER, _scanLinesNumberCRT);
         _volume.sharedProfile = _crtProfile;
     }
 
-    public void SetDefaultFilter()
+    private void SetDefaultFilter()
     {
         _crtMaterial.SetFloat(SCAN_LINE_NUMBER, _scanLinesNumberDefault);
         _volume.sharedProfile = _defaultProfile;
@@ -42,10 +36,23 @@ public class FilterManager : MonoBehaviour
 
     public void ChangeFilter(bool isOn)
     {
-        Debug.Log(isOn);
         if (isOn)
             SetCRTFilter();
         else
             SetDefaultFilter();
+        SaveFilterPrefs(isOn);
+    }
+
+    private void LoadFilterPrefs()
+    {
+        bool useCRT = PlayerPrefs.GetInt(PREF_KEY, 1) == 1;
+        _toggle.isOn = useCRT;
+        ChangeFilter(useCRT);
+    }
+
+    private void SaveFilterPrefs(bool useCRT)
+    {
+        PlayerPrefs.SetInt(PREF_KEY, useCRT ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
